50587f3dc9d7915b9c5b9604d69fb4e4bab703b8
Vlad Yarema <exsroso9ik@gmail.com>
2017-07-01 12:45:10 +0300

    I see lesson about route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('page/', function () {
-    return view('page');
+Route::get('/page', function () {
+    echo '<pre>';
+    //print_r($_ENV);
+    //echo config('app.php.locale');
+    //echo Config::set('app.locale', "ru");
+    //echo Config::get('app.locale');
+    echo env("APP_ENV");
+    echo '</pre>';
+});
+
+
+Route::post('/test', function () {
+    echo "привет мир!";
 });
